10a8866ff8fbd7317783fabeaaf5149513a0903a
hasinduudara <hasindutechschool@gmail.com>
2025-12-10 22:24:21 +0530

    feat: increase number of crypto coins fetched from API to 20

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     const fetchCrypto = async () => {
         try {
             const res = await fetch(
-                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1"
+                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
             );
             const data = await res.json();
             setCoins(data);
